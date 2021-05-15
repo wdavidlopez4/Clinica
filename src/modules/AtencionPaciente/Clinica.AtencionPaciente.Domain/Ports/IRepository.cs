b5213765356ff925c37cb0deec53f67acd175bef
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-05-15 18:29:13 -0500

    creacion el auto mapeo de objetos y del los metodos de "guardar" "existe" del repositorio interface

--- src/modules/AtencionPaciente/Clinica.AtencionPaciente.Domain/Ports/IRepository.cs
+++ src/modules/AtencionPaciente/Clinica.AtencionPaciente.Domain/Ports/IRepository.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using Clinica.AtencionPaciente.Domain.Entities;
+using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Clinica.AtencionPaciente.Domain.Ports
 {
     public interface IRepository
     {
-        
+        /// <summary>
+        /// almacena el objeto en la db
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public Task<T> Save<T>(T obj, CancellationToken cancellationToken) where T : Entity;
+
+        /// <summary>
+        /// verificar si existe una entidad
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expression"></param>
+        /// <returns></returns>
+        public bool Exists<T>(Expression<Func<T, bool>> expression) where T : Entity;
     }
 }
