dee11125c6492d00d14e803efe757ef0590e3798
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-05-15 22:42:46 -0500

    creacion del context

---
+++ src/modules/AtencionPaciente/Clinica.AtencionPaciente.Infrastructure/EFCore/AtencionPacientesContex.cs
@@ -0,0 +1,29 @@
+﻿using Clinica.AtencionPaciente.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clinica.AtencionPaciente.Infrastructure.EFCore
+{
+    public class AtencionPacientesContex : DbContext
+    {
+        public DbSet<Anciano> Ancianos  { get; set; }
+
+        public DbSet<ConsultaClinica> ConsultasClinicas  { get; set; }
+
+        public DbSet<Especialista> Especialistas  { get; set; }
+
+        public DbSet<Hospital> Hospitales  { get; set; }
+
+        public DbSet<Joven> Jovenes  { get; set; }
+
+        public DbSet<Ninno> Ninnos  { get; set; }
+
+        public AtencionPacientesContex(DbContextOptions<AtencionPacientesContex> options) : base(options)
+        {
+            
+        }
+
+    }
+}
