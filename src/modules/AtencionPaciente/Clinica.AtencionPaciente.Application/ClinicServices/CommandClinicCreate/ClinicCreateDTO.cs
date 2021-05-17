f8bb5d84e3ffb54f587f031b782381568034e49f
William David Lopez Sanchez <wdavidlopez4@gmail.com>
2021-05-17 16:20:36 -0500

    creacion del componente para crear la consulta clinica.

---
+++ src/modules/AtencionPaciente/Clinica.AtencionPaciente.Application/ClinicServices/CommandClinicCreate/ClinicCreateDTO.cs
@@ -0,0 +1,25 @@
+﻿using Clinica.AtencionPaciente.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Clinica.AtencionPaciente.Application.ClinicServices.CommandClinicCreate
+{
+    public class ClinicCreateDTO
+    {
+        public string Id { get; set; }
+
+        public int CantidadPacientes { get; set; }
+
+        public TipoConsultaEnum TipoConsulta { get; set; }
+
+        public EstadoEnum Estado { get; set; }
+
+        public class Especialista
+        {
+            public string Id { get; set; }
+
+            public string Nombre { get; set; }
+        }
+    }
+}
